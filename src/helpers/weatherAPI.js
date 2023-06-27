a55067d367b84d27205cf03ec5e99dda002f0aed
Luciano Prado <lucianobatuta@live.com>
2023-06-27 18:31:18 -0300

    Implementa a função searchCities

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,19 @@
 // Remova os comentários a medida que for implementando as funções
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = (term) => {
+  return fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${import.meta.env.VITE_TOKEN}&q=${term}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length === 0) {
+        window.alert('Nenhuma cidade encontrada');
+        return [];
+      }
+      return data;
+    })
+    .catch((error) => {
+      console.error('Ocorreu um erro em sua requisição:', error);
+      return [];
+    });
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
